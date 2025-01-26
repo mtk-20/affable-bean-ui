c74302f26465bbcea997844d210a394860ab57e3
mtk <thukhamyat2000@gmail.com>
2025-01-26 12:07:55 +0630

    frontend

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,62 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+      <>
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <h3>Welcome to the online home of the Affable Bean Grocery.</h3>
+              <br/>
+              <p className="lead">Enjoy browsing and learning more about our unique home delivery service
+                bringing
+                you fresh organic product,dairy,meat,bread and other delicious and healthy items to your
+                doorstep.
+              </p>
+            </div>
+            <div className="col mt-2">
+              <div className="row">
+                <div className="col">
+                  <div className="card">
+                    <Link href="/products/1">
+                      <Image src="/images/categories/dairy.jpg" alt="dairy image"
+                             className="card-img-top shadow-lg" width={350} height={230}/>
+                    </Link>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+                  </div>
+                </div>
+                <div className="col">
+                  <div className="card">
+                    <Link href="/products/2">
+                      <Image src="/images/categories/meats.jpg" alt="meats image"
+                             className="card-img-top shadow-lg" width={350} height={230}/>
+                    </Link>
+
+                  </div>
+                </div>
+              </div>
+              <div className="row mt-3">
+                <div className="col">
+                  <div className="card">
+                    <Link href="/products/3">
+                      <Image src="/images/categories/bakery.jpg" alt="bakery image"
+                             className="card-img-top shadow-lg" width={350} height={230}/>
+                    </Link>
+
+                  </div>
+                </div>
+                <div className="col">
+                  <div className="card">
+                    <Link href="/products/4">
+                      <Image src="/images/categories/fruit_veg.jpg" alt="fruit_veg image"
+                             className="card-img-top shadow-lg" width={350} height={230}/>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+      </>);
 }
